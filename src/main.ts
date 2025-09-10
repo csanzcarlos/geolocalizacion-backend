ab1e7600f4233dce61661d635bfcc64028781fa2
carlos sanz <csanzcarlos@gmail.comm>
2025-09-09 23:47:19 -0400

    feat: Add Empresa entity

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ ESTA LÍNEA SOLUCIONA EL ERROR DE CORS
   app.enableCors(); 
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
