0b27cad11a13646dcb5fc749f409f64ae2fffdfb
carlos sanz <csanzcarlos@gmail.comm>
2025-09-19 14:29:54 -0400

    feat: Add whatsapp entity

--- src/whatsapp/dto/conectar-numero.dto.ts
+++ src/whatsapp/dto/conectar-numero.dto.ts
@@ -2,5 +2,5 @@ export class ConectarNumeroDto {
   nombre_agente: string;
   numero_whatsapp: string;
   usuario_id: string;
-  alerta_timeout?: number; // Opcional, tiempo para alerta en segundos
+  alerta_timeout?: number; // Opcio
 }
