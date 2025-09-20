989d4acd79c1b5096e97468751d059a2815bba23
carlos sanz <csanzcarlos@gmail.comm>
2025-09-20 11:55:34 -0400

    feat: Add src entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,18 @@
-// src/data-source.ts
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); // Carga las variables de tu archivo .env
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-
-  // ✅ Usamos la URL de conexión directamente de tu archivo .env
-  url: process.env.DATABASE_URL,
-
-  // ✅ Añadimos configuración SSL necesaria para conectar con Render
-  ssl: {
-    rejectUnauthorized: false,
-  },
-
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
-  synchronize: false, // ¡Muy importante que sea false!
+  synchronize: false,
 };
 
 const AppDataSource = new DataSource(dataSourceOptions);
